cb186929483743a2acbad8ec5799da7195e73fc8
Egzon Thaqi <egzonthaqi99@gmail.com>
2022-09-04 18:32:07 +0200

    MongoDB connection| Paypal and StripePayment|Final

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import React from 'react';
 
-export default function ProductItem({ product }) {
+export default function ProductItem({ product, addToCartHandler }) {
   return (
     <div className="card">
       <Link href={`/product/${product.slug}`}>
@@ -10,7 +10,7 @@ export default function ProductItem({ product }) {
           <img
             src={product.image}
             alt={product.name}
-            className="rounded shadow"
+            className="rounded shadow object-cover h-64 w-full"
           />
         </a>
       </Link>
@@ -22,9 +22,13 @@ export default function ProductItem({ product }) {
         </Link>
         <p className="mb-2">{product.brand}</p>
         <p>${product.price}</p>
-        <buton className="primary-button" type="button">
-          Add to Cart
-        </buton>
+        <button
+          className="primary-button"
+          type="button"
+          onClick={() => addToCartHandler(product)}
+        >
+          Add to cart
+        </button>
       </div>
     </div>
   );
